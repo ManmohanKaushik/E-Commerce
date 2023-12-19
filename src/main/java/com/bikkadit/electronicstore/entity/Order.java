c44cc7ff3d14781cebdc6bc3ceb2a6616bbc1ed0
SIYARAM <manmohankaushik76035@gmail.com>
2023-12-19 17:23:29 +0530

    My  19 Dec 2023 commit  order & orderItem entity ,services, repositroy,createMethod created but mapping is not working

--- src/main/java/com/bikkadit/electronicstore/entity/Order.java
+++ src/main/java/com/bikkadit/electronicstore/entity/Order.java
@@ -7,40 +7,38 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-//@Entity
-//@Table(name ="Order")
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@Builder
+
+@Entity
+@Table(name = "order_details")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Order {
+    @Id
+    private String orderId;
 
-//
-//  @Id
-//  private String orderId;
-//
-//  private String orderStatus;
-//
-//  private String paymentStatus;
-//
-//  private Integer orderAmount;
-//
-//  private String billingAddress;
-//
-//  private String billingPhone;
-//
-//  private String billingName;
-//
-//  private Date orderedDate;
-//
-//  private Date deliveredDate;
-//
-//  @ManyToOne(fetch = FetchType.EAGER)
-//  @JoinColumn(name = "user_id")
-//  private User user;
-//
-//  @OneToMany(mappedBy = "order",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
-//  private List<OrderItem> orderItems=new ArrayList<>();
-  }
+    private String orderStatus;
 
+    private String paymentStatus;
+
+    private Integer orderAmount;
+
+    private String billingAddress;
+
+    private String billingPhone;
+
+    private String billingName;
+
+    private Date orderedDate;
+
+    private Date deliveredDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems=new ArrayList<>();
+}
